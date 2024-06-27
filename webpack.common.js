612c607e9fb58773ccf9a85d74cf07afdb202ead
man7788 <jerman1841@gmail.com>
2024-06-28 02:07:49 +0800

    Update webpack configs

--- webpack.common.js
+++ webpack.common.js
@@ -2,13 +2,7 @@ const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
-  mode: 'development',
   entry: './src/index.js',
-  devtool: 'inline-source-map',
-  devServer: {
-    static: './dist',
-    watchFiles: ['./src/template.html'],
-  },
   plugins: [
     new HtmlWebpackPlugin({
       template: './src/template.html',
@@ -36,8 +30,5 @@ module.exports = {
         type: 'asset/resource',
       },
     ],
-  },
-  optimization: {
-    runtimeChunk: 'single',
   },
 };
